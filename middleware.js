8c9e0aa396e948a91bce8caceff4ef3699b709e9
hermesthemes <121753577+HermesThemes@users.noreply.github.com>
2024-01-13 18:40:03 +0000

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server'
 
 
 export default withAuth(function middleware(req){
-        secret: process.env.SECRET
+        secret: process.env.NEXTAUTH_SECRET
         if(req.nextUrl.pathname === "/admin" && req.nextauth.token.role === "user"){
             
             return NextResponse.redirect(req.nextUrl.origin)
