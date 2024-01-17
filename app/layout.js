1e5295eb7402bf9664c98a21c75937911fc24b64
hermesthemes <121753577+HermesThemes@users.noreply.github.com>
2024-01-17 12:12:53 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -27,6 +27,7 @@ export default function RootLayout({ children }) {
               </div>
         </TanstackProvider>
       </NextAuthProvider>
+      <script type="module" src="https://cdn.jsdelivr.net/npm/ldrs/dist/auto/ping.js"></script>
       </body>
     </html>
   )
