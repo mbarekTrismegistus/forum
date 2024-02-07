a0dfdc59e7189e23f52bb5b20fe340e316697f5b
DWFS202L <momoboogeyman2000@gmail.com>
2024-02-07 11:22:11 +0100

    speed insight

--- middleware.js
+++ middleware.js
@@ -5,6 +5,8 @@ import { NextResponse } from 'next/server'
 
 export default withAuth(function middleware(req){
         secret: process.env.NEXTAUTH_SECRET
+        console.log(req.nextUrl.pathname)
+        console.log(req.nextUrl.pathname.startsWith("/admin",))
         if(req.nextUrl.pathname.startsWith("/admin") && req.nextauth.token.role === "user"){
             
             return NextResponse.redirect(req.nextUrl.origin)
